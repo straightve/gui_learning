0a8913ff09f31c4f0c2538de653c06ffe4c7a4d9
straightve <straightve@wgc.school.nz>
2019-02-15 10:02:47 +1300

    added colours and fonts

--- hellow_world.py
+++ hellow_world.py
@@ -10,11 +10,11 @@ my_label.config(text="Hello!", fg="green")
 my_label.grid()
 
 my_label2 = tkinter.Label(root)
-my_label2.config(text="hey there!", fg="blue")
+my_label2.config(text="hey there!", fg="blue", font=("Wingdings", "16"))
 my_label2.grid()
 
 my_label3 = tkinter.Label(root)
-my_label3.config(text="hey you", fg="purple")
+my_label3.config(text="hey you!", fg="green", bg="blue", width="5", font=("Wingdings", "40", "bold"))
 my_label3.grid()
 
 root.mainloop()
