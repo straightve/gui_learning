b729511fcfce9c07fbf1421f73f85fca02ff1822
straightve <straightve@wgc.school.nz>
2019-02-14 15:15:42 +1300

    added more labels

--- hellow_world.py
+++ hellow_world.py
@@ -9,4 +9,12 @@ my_label = tkinter.Label(root)
 my_label.config(text="Hello!", fg="green")
 my_label.grid()
 
+my_label2 = tkinter.Label(root)
+my_label2.config(text="hey there!", fg="blue")
+my_label2.grid()
+
+my_label3 = tkinter.Label(root)
+my_label3.config(text="hey you", fg="purple")
+my_label3.grid()
+
 root.mainloop()
