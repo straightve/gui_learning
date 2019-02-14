879ccdbbaf06b0c922826aeaef3b358decd41179
straightve <straightve@wgc.school.nz>
2019-02-14 15:14:45 +1300

    added lable

--- hellow_world.py
+++ hellow_world.py
@@ -1,7 +1,12 @@
 import tkinter
+
 root = tkinter.Tk()
 
 root.title("First app!!")
 root.geometry("800x600")
+
+my_label = tkinter.Label(root)
+my_label.config(text="Hello!", fg="green")
+my_label.grid()
 
 root.mainloop()
